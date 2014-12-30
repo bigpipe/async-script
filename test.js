6bf7c8843f5fe72d22295bfb888cf21e7cfbd629
Arnout Kazemier <info@3rd-Eden.com>
2014-12-30 23:07:33 +0100

    [test] Adding loading tests

--- test.js
+++ test.js
@@ -1,10 +1,32 @@
 describe('async-script', function () {
   'use strict';
 
-  var assume = require('assume')
+  var url = 'https://code.jquery.com/jquery-latest.js'
+    , assume = require('assume')
     , script = require('./');
 
   it('is exported as function', function () {
     assume(script).is.a('function');
+  });
+
+  it('loads the script', function (next) {
+    assume(typeof jQuery).equals('undefined');
+
+    script(document, url, function (err) {
+      if (err) return next(err);
+
+      assume(typeof jQuery).does.not.equal('undefined');
+      next();
+    });
+  });
+
+  it('returns a unload function when called', function (next) {
+    var unload = script(document, url, function (err) {
+      assume(err.message).includes('remove');
+      next();
+    });
+
+    assume(unload).is.a('function');
+    unload();
   });
 });
